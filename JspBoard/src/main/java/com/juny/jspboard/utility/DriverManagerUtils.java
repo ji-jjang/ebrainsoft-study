d3417879daf6780c9d2561787b11082423ccf56c
jinyus <jinyhehe@gmail.com>
2024-11-05 17:59:26 +0900

    feat: Connection 얻는 유틸 함수 추가 및 시간 변환 유틸 클래스 추가

--- JspBoard/src/main/java/com/juny/jspboard/utility/DriverManagerUtils.java
+++ JspBoard/src/main/java/com/juny/jspboard/utility/DriverManagerUtils.java
@@ -4,12 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// TODO 1.상수화. 2. .env 파일을 읽어서 설정할 수 있도록
+
+/**
+ * The type Driver manager utils.
+ */
 public class DriverManagerUtils {
 
   private static final String DB_URL = "jdbc:mysql://localhost:3306/ebrainsoft_study";
   private static final String USER = "ebsoft";
   private static final String PASS = "ebsoft";
 
+  /**
+   * DB 연결 얻는 메서드
+   *
+   * @return the connection
+   * @throws ClassNotFoundException the class not found exception
+   */
   public static Connection getConnection() throws ClassNotFoundException {
 
     Connection conn = null;
