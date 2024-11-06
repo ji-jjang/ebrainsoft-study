2f5ca0fc234095cdc29de10a62738a46a291e51a
jinyus <jinyhehe@gmail.com>
2024-11-06 16:17:26 +0900

    feat: 상세페이지 조회시 조회수 추가, 상세 페이지 화면 조회, 댓글 생성 DAO 구현

--- JspBoard/src/main/java/com/juny/jspboard/dao/BoardDAO.java
+++ JspBoard/src/main/java/com/juny/jspboard/dao/BoardDAO.java
@@ -1,6 +1,7 @@
 package com.juny.jspboard.dao;
 
 import com.juny.jspboard.dto.ReqBoard;
+import com.juny.jspboard.dto.ResBoardDetail;
 import com.juny.jspboard.dto.ResBoardViewList;
 import java.util.List;
 import java.util.Map;
@@ -8,7 +9,7 @@ import java.util.Map;
 public interface BoardDAO {
   ResBoardViewList createBoard(ReqBoard req);
   List<ResBoardViewList> getBoardList(int page);
-  ResBoardViewList getBoardDetail(int boardId);
+  ResBoardDetail getBoardDetail(int boardId);
   ResBoardViewList updateBoard(int boardId);
   void deleteBoard(int boardId);
 
@@ -18,5 +19,9 @@ public interface BoardDAO {
   int getTotals();
 
   int getTotalsWithSearchConditions(Map<String, String> searchConditions);
+
+  void increaseViewCount(int boardId);
+
+  void createComment(int boardId, String name, String password, String content);
 }
 
--- JspBoard/src/main/java/com/juny/jspboard/dao/BoardDAOImpl.java
+++ JspBoard/src/main/java/com/juny/jspboard/dao/BoardDAOImpl.java
@@ -1,14 +1,17 @@
 package com.juny.jspboard.dao;
 
 import com.juny.jspboard.dto.ReqBoard;
+import com.juny.jspboard.dto.ResAttachment;
+import com.juny.jspboard.dto.ResBoardDetail;
+import com.juny.jspboard.dto.ResBoardImage;
 import com.juny.jspboard.dto.ResBoardViewList;
+import com.juny.jspboard.dto.ResComment;
 import com.juny.jspboard.utility.DriverManagerUtils;
 import com.juny.jspboard.utility.TimeFormatterUtils;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -78,9 +81,116 @@ public class BoardDAOImpl implements BoardDAO {
   }
 
   @Override
-  public ResBoardViewList getBoardDetail(int boardId) {
-    return null;
+  public ResBoardDetail getBoardDetail(int boardId) {
+
+    ResBoardDetail board = null;
+
+    String sql =
+      "SELECT b.id, b.title, b.created_by, b.created_at, b.updated_at, b.view_count, b.content, c.name AS category_name " +
+        "FROM boards b " +
+        "LEFT JOIN categories c ON b.category_id = c.id " +
+        "WHERE b.id = ?";
+
+    try (Connection conn = DriverManagerUtils.getConnection();
+      PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+      pstmt.setLong(1, boardId);
+      ResultSet rs = pstmt.executeQuery();
+
+      if (rs.next()) {
+
+        Long id = rs.getLong("id");
+        String title = rs.getString("title");
+        String createdBy = rs.getString("created_by");
+        String createdAt = TimeFormatterUtils.datetimeToString(rs.getTimestamp("created_at").toLocalDateTime());
+        String updatedAt = "-";
+        if (rs.getTimestamp("updated_at") != null) {
+          updatedAt = TimeFormatterUtils.datetimeToString(rs.getTimestamp("updated_at").toLocalDateTime());
+        }
+        int viewCount = rs.getInt("view_count");
+        String content = rs.getString("content");
+        String categoryName = rs.getString("category_name");
+
+        List<ResBoardImage> boardImages = getBoardImages(conn, boardId);
+        List<ResAttachment> attachments = getAttachments(conn, boardId);
+        List<ResComment> comments = getComments(conn, boardId);
+
+        board =
+          new ResBoardDetail(
+            id,
+            title,
+            createdBy,
+            createdAt,
+            updatedAt,
+            viewCount,
+            content,
+            categoryName,
+            boardImages,
+            attachments,
+            comments);
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+    return board;
   }
+
+  private List<ResAttachment> getAttachments(Connection conn, int boardId) {
+
+    List<ResAttachment> attachments = new ArrayList<>();
+
+    String sql = "SELECT logical_name, stored_name, stored_path, extension FROM attachments WHERE board_id = ?";
+
+    try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+      pstmt.setLong(1, boardId);
+      ResultSet rs = pstmt.executeQuery();
+      while (rs.next()) {
+        attachments.add(new ResAttachment(rs.getString("logical_name"), rs.getString("stored_name"), rs.getString("stored_path"), rs.getString("extension")));
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return attachments;
+  }
+
+  private List<ResComment> getComments(Connection conn, int boardId) {
+
+    List<ResComment> comments = new ArrayList<>();
+
+    String sql = "SELECT content, created_at, created_by FROM comments WHERE board_id = ? ORDER BY created_at ASC";
+
+    try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+      pstmt.setLong(1, boardId);
+      ResultSet rs = pstmt.executeQuery();
+      while (rs.next()) {
+        comments.add(new ResComment(rs.getString("content"), rs.getString("created_at"), rs.getString("created_by")));
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return comments;
+  }
+
+  private List<ResBoardImage> getBoardImages(Connection conn, int boardId) {
+
+    List<ResBoardImage> images = new ArrayList<>();
+    String sql = "SELECT stored_path, stored_name, extension FROM board_images WHERE board_id = ?";
+
+    try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+      pstmt.setLong(1, boardId);
+      ResultSet rs = pstmt.executeQuery();
+      while (rs.next()) {
+        images.add(new ResBoardImage(rs.getString("stored_path"), rs.getString("stored_name"), rs.getString("extension")));
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return images;
+  }
+
+
 
   @Override
   public ResBoardViewList updateBoard(int boardId) {
@@ -284,7 +394,7 @@ public class BoardDAOImpl implements BoardDAO {
     }
 
     try (Connection conn = DriverManagerUtils.getConnection();
-        PreparedStatement pstmt = conn.prepareStatement(sql.toString())) {
+      PreparedStatement pstmt = conn.prepareStatement(sql.toString())) {
 
       int index = 0;
       if (searchConditions.containsKey("startDate") && searchConditions.containsKey("endDate")) {
@@ -312,6 +422,43 @@ public class BoardDAOImpl implements BoardDAO {
       throw new RuntimeException(e);
     }
     return 0;
+  }
+
+  @Override
+  public void increaseViewCount(int boardId) {
+
+    String sql = "UPDATE boards SET view_count = view_count + 1 WHERE id = ?";
+
+    try (Connection conn = DriverManagerUtils.getConnection();
+      PreparedStatement pstmt = conn.prepareStatement(sql)) {
+      pstmt.setLong(1, boardId);
+      pstmt.executeUpdate();
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void createComment(int boardId, String name, String password, String content) {
+
+    String sql = "INSERT INTO comments (board_id, created_by, password, content, created_at) VALUES (?, ?, ?, ?, NOW())";
+
+    try (Connection conn = DriverManagerUtils.getConnection();
+      PreparedStatement pstmt = conn.prepareStatement(sql)) {
+      pstmt.setLong(1, boardId);
+      pstmt.setString(2, name);
+      pstmt.setString(3, password);
+      pstmt.setString(4, content);
+      pstmt.executeUpdate();
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   private String handleLongTitle(String title) {
