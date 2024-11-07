d64476c1d893dd5219bedc6e072efae47678a0b2
jinyus <jinyhehe@gmail.com>
2024-11-07 14:02:19 +0900

    feat: 게시판 등록 버튼 추가, 게시판 등록 폼 작성, 게시판 등록 서블릿(핸들러) 추가

--- JspBoard/src/main/java/com/juny/jspboard/servlet/CreateBoardServlet.java
+++ JspBoard/src/main/java/com/juny/jspboard/servlet/CreateBoardServlet.java
@@ -1,16 +1,24 @@
 package com.juny.jspboard.servlet;
 
+import com.juny.jspboard.dao.BoardDAO;
+import com.juny.jspboard.dao.BoardDAOImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class CreateBoardServlet implements BoardControllerServlet {
+
+  private final BoardDAO boardDAO = new BoardDAOImpl();
 
   @Override
   public void execute(HttpServletRequest req, HttpServletResponse res)
     throws ServletException, IOException {
 
-    System.out.println("CreateBoardServlet.execute");
+    List<String> categories = boardDAO.getCategories();
+    req.setAttribute("categories", categories);
+
+    req.getRequestDispatcher("/createBoard.jsp").forward(req, res);
   }
 }
