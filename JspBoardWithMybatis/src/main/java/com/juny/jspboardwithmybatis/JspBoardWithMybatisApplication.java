5231ad6b6890c0a1d5583b6ac6224011fe4ca347
jinyus <jinyhehe@gmail.com>
2024-11-18 16:59:05 +0900

    feat: init 프로젝트, Mybatis 환경 설정 및 Local, TEST 환경 구축

---
+++ JspBoardWithMybatis/src/main/java/com/juny/jspboardwithmybatis/JspBoardWithMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.juny.jspboardwithmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspBoardWithMybatisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JspBoardWithMybatisApplication.class, args);
+  }
+}
---
+++ JspBoardWithMybatis/src/test/java/com/juny/jspboardwithmybatis/JspBoardWithMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juny.jspboardwithmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspBoardWithMybatisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
