389a668a6dcc7dcfd769925bf89826da18c849ea
jinyus <jinyhehe@gmail.com>
2024-11-20 20:33:34 +0900

    feat: 카테고리 이름으로 id 찾는 메서드 추가

--- JspBoardWithMybatis/src/main/java/com/juny/jspboardwithmybatis/domain/utils/CategoryMapperUtils.java
+++ JspBoardWithMybatis/src/main/java/com/juny/jspboardwithmybatis/domain/utils/CategoryMapperUtils.java
@@ -1,11 +1,11 @@
 package com.juny.jspboardwithmybatis.domain.utils;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/** 간단한 카테고리 목록, DB 조회보다는 정적 메모리에서 가져오기 위함 */
 public class CategoryMapperUtils {
 
   private static final Map<Long, String> CATEGORY_MAP = new HashMap<>();
@@ -16,16 +16,30 @@ public class CategoryMapperUtils {
     CATEGORY_MAP.put(3L, "Database");
   }
 
-  public static String getCategoryNameById(long id) {
-
-    return CATEGORY_MAP.get(id);
+  /**
+   *
+   *
+   * <h1>카테고리 이름으로 아이디 찾기</h1>
+   *
+   * @param categoryName
+   * @return categoryId
+   */
+  public static Long getCategoryIdByName(String categoryName) {
+    for (var entry : CATEGORY_MAP.entrySet()) {
+      if (entry.getValue().equals(categoryName)) {
+        return entry.getKey();
+      }
+    }
+    return null;
   }
 
-  public static Map<Long, String> getCategoryMap() {
-
-    return Collections.unmodifiableMap(CATEGORY_MAP);
-  }
-
+  /**
+   *
+   *
+   * <h1>카테고리 모든 목록 찾기(게시판 생성 폼)</h1>
+   *
+   * @return all categories name
+   */
   public static List<String> getAllCategoryName() {
 
     return new ArrayList<>(CATEGORY_MAP.values());
