3bf00271f41538ab9125c98294221adecbd083fd
jinyus <jinyhehe@gmail.com>
2024-11-21 18:30:40 +0900

    refactor: 트랜잭션 효율적 처리 위해 파일 파싱, 파일 저장하는 로직을 분리

--- JspBoardWithMybatis/src/main/java/com/juny/jspboardwithmybatis/domain/utils/FileUtils.java
+++ JspBoardWithMybatis/src/main/java/com/juny/jspboardwithmybatis/domain/utils/FileUtils.java
@@ -12,6 +12,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.IntStream;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUtils {
@@ -54,33 +55,75 @@ public class FileUtils {
    *
    * <h1>파일 세부 정보 저장</h1>
    *
+   * - MultipartFile 파일이 없어도 빈 객체로 전달
+   *
    * @param files
    * @param path
    * @return FileDetails
    */
-  public static List<FileDetails> saveFileDetails(List<MultipartFile> files, String path) {
+  public static List<FileDetails> parseFileDetails(List<MultipartFile> files, String path) {
 
     String storedPath = "/Users/jijunhyuk/JunyProjects/ebrainsoft/" + path + "/";
     List<FileDetails> fileDetailsList = new ArrayList<>();
 
     for (var file : files) {
+
+      if (file.isEmpty()) {
+        continue;
+      }
+
       String extension = "";
       String fileName = file.getOriginalFilename();
       String storedName = UUID.randomUUID().toString().replace("-", "");
       if (fileName.contains(".")) {
         extension = fileName.substring(fileName.lastIndexOf(".") + 1);
       }
-      try {
-        String delimiter = ".";
-        if (extension.equals("")) delimiter = "";
-
-        file.transferTo(Path.of(storedPath + storedName + delimiter + extension));
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
       fileDetailsList.add(
           new FileDetails(fileName, storedName, storedPath, extension, file.getSize()));
     }
     return fileDetailsList;
+  }
+
+  /**
+   *
+   *
+   * <h1>파일 시스템에 파일 저장</h1>
+   *
+   * <br>
+   * - MultipartFile 파일이 없어도 빈 객체로 전달
+   *
+   * @param files
+   * @param fileDetailsList
+   */
+  public static void saveFile(List<MultipartFile> files, List<FileDetails> fileDetailsList) {
+
+    IntStream.range(0, fileDetailsList.size())
+        .forEach(
+            idx -> {
+              var file = files.get(idx);
+              var fileDetails = fileDetailsList.get(idx);
+              String extension = fileDetails.getExtension();
+              String storedName = fileDetails.getStoredName();
+              String storedPath = fileDetails.getStoredPath();
+              String delimiter = ".";
+              if (fileDetails.getExtension().equals("")) delimiter = "";
+
+              try {
+                file.transferTo(Paths.get(storedPath, storedName + delimiter + extension));
+              } catch (IOException e) {
+                throw new RuntimeException(e);
+              }
+            });
+  }
+
+  public static void deleteFile(List<String> deleteFilePaths) {
+
+    for (var path : deleteFilePaths) {
+      try {
+        Files.delete(Path.of(path));
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
 }
