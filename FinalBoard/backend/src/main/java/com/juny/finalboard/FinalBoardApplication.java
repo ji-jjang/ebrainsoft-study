38219a7f000ba233c9e0c359981a09a64ff55a03
jinyus <jinyhehe@gmail.com>
2024-12-11 16:56:49 +0900

    feat: 백엔드 프로젝트 초기 환경 구성 및 필요한 의존성 추가

---
+++ FinalBoard/backend/src/main/java/com/juny/finalboard/FinalBoardApplication.java
@@ -0,0 +1,12 @@
+package com.juny.finalboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBoardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinalBoardApplication.class, args);
+  }
+}
